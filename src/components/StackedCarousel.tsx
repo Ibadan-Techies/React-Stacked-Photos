75f946347d2a889e52ff2edf086ed558a00b067b
kelvinsekx <utdkelvin@gmail.com>
2023-05-15 03:42:38 -0700

    remove forwardRef

--- src/components/StackedCarousel.tsx
+++ src/components/StackedCarousel.tsx
@@ -1,109 +1,111 @@
-import * as React from "react";
-import "./stackedCarousel.scss";
+import * as React from 'react'
+import './stackedCarousel.scss'
 
-type TReactStackedPhotos = {
-  width?: number;
-  height?: number;
-  children: React.ReactElement;
-};
+interface TReactStackedPhotos {
+  width?: number
+  height?: number
+  children: React.ReactElement
+}
 
-const { useState, useRef, useEffect, forwardRef } = React;
-export const ReactStackedPhotos: React.FC<TReactStackedPhotos> = forwardRef(
-  ({ children, width, height }): JSX.Element => {
-    const [isHovering, setIsHovering] = useState(false);
-    const imagesRef = useRef<
-      HTMLDivElement & { lastElementChild: HTMLElement | null }
-    >(null);
-    const timerRef1 = useRef<ReturnType<typeof setTimeout> | string>();
-    const timerRef2 = useRef<ReturnType<typeof setTimeout> | string>();
+const { useState, useRef, useEffect } = React
+export const ReactStackedPhotos: React.FC<TReactStackedPhotos> = ({
+  children,
+  width,
+  height
+}): JSX.Element => {
+  const [isHovering, setIsHovering] = useState(false)
+  const imagesRef = useRef<
+  HTMLDivElement & { lastElementChild: HTMLElement | null }
+  >(null)
+  const timerRef1 = useRef<ReturnType<typeof setTimeout> | string>()
+  const timerRef2 = useRef<ReturnType<typeof setTimeout> | string>()
 
-    const [active, setActive] = useState(false);
-    const [last, setLast] = useState<DOMTokenList | null | undefined>(null);
+  const [active, setActive] = useState(false)
+  const [last, setLast] = useState<DOMTokenList | null | undefined>(null)
 
-    const handleMouseOver = (): void => {
-      setIsHovering((prev) => !prev);
-    };
-
-    useEffect(() => {
-      setLast(
-        imagesRef.current != null
-          ? imagesRef.current.lastElementChild?.classList
-          : null
-      );
-      return () => {
-        clearTimeout(timerRef1.current);
-        clearTimeout(timerRef2.current);
-      };
-    }, []);
-
-    const handleOnClick = (event: React.MouseEvent<HTMLDivElement>): void => {
-      if (!active) {
-        setActive(true);
-
-        const clickTarget = imagesRef.current;
-        // What side of the element was clicked. Read more
-        // https://stackoverflow.com/questions/15685708/determining-if-mouse-click-happened-in-left-or-right-half-of-div
-        const clickTargetWidth = clickTarget?.offsetWidth;
-        const xCoordInClickTarget =
-          event?.clientX - Number(clickTarget?.offsetLeft);
-
-        let sideClicked = "";
-        if (Number(clickTargetWidth) / 2 > xCoordInClickTarget) {
-          // clicked left
-          sideClicked = "slide-left";
-          last?.add("slide-left");
-        } else {
-          // clicked right
-          sideClicked = "slide-right";
-          last?.add("slide-right");
-        }
-
-        timerRef1.current = setTimeout(function () {
-          last?.remove(sideClicked);
-          last?.add("back");
-
-          timerRef2.current = setTimeout(function () {
-            imagesRef.current?.insertBefore(
-              imagesRef.current?.lastElementChild as Node,
-              imagesRef.current?.firstElementChild
-            );
-            last?.remove("back");
-            setLast(imagesRef.current?.lastElementChild?.classList);
-            setActive(false);
-          }, 400);
-        }, 500);
-      }
-    };
-
-    return (
-      <div
-        className="_9123_imageWrapper"
-        ref={imagesRef}
-        onClick={handleOnClick}
-        style={{ width, height }}
-      >
-        {React.Children.map(
-          React.Children.toArray(children),
-          (child: React.ReactElement, index) => {
-            const { isHoveringStyles, defaultStyles, style, ...rest } =
-              child.props;
-            console.log(rest);
-            return (
-              <div
-                key={index}
-                className={"img "}
-                {...rest}
-                style={Object.assign(
-                  { ...style },
-                  isHovering ? { ...isHoveringStyles } : {}
-                )}
-                onMouseOver={handleMouseOver}
-                onMouseOut={handleMouseOver}
-              ></div>
-            );
-          }
-        )}
-      </div>
-    );
+  const handleMouseOver = (): void => {
+    setIsHovering((prev) => !prev)
   }
-);
+
+  useEffect(() => {
+    setLast(
+      imagesRef.current != null
+        ? imagesRef.current.lastElementChild?.classList
+        : null
+    )
+    return () => {
+      clearTimeout(timerRef1.current)
+      clearTimeout(timerRef2.current)
+    }
+  }, [])
+
+  const handleOnClick = (event: React.MouseEvent<HTMLDivElement>): void => {
+    if (!active) {
+      setActive(true)
+
+      const clickTarget = imagesRef.current
+      // What side of the element was clicked. Read more
+      // https://stackoverflow.com/questions/15685708/determining-if-mouse-click-happened-in-left-or-right-half-of-div
+      const clickTargetWidth = clickTarget?.offsetWidth
+      const xCoordInClickTarget =
+        event?.clientX - Number(clickTarget?.offsetLeft)
+
+      let sideClicked = ''
+      if (Number(clickTargetWidth) / 2 > xCoordInClickTarget) {
+        // clicked left
+        sideClicked = 'slide-left'
+        last?.add('slide-left')
+      } else {
+        // clicked right
+        sideClicked = 'slide-right'
+        last?.add('slide-right')
+      }
+
+      timerRef1.current = setTimeout(function () {
+        last?.remove(sideClicked)
+        last?.add('back')
+
+        timerRef2.current = setTimeout(function () {
+          imagesRef.current?.insertBefore(
+            imagesRef.current?.lastElementChild as Node,
+            imagesRef.current?.firstElementChild
+          )
+          last?.remove('back')
+          setLast(imagesRef.current?.lastElementChild?.classList)
+          setActive(false)
+        }, 400)
+      }, 500)
+    }
+  }
+
+  return (
+    <div
+      className="_9123_imageWrapper"
+      ref={imagesRef}
+      onClick={handleOnClick}
+      style={{ width, height }}
+    >
+      {React.Children.map(
+        React.Children.toArray(children),
+        (child: React.ReactElement, index) => {
+          const { isHoveringStyles, defaultStyles, style, ...rest } =
+            child.props
+          console.log(rest)
+          return (
+            <div
+              key={index}
+              className={'img '}
+              {...rest}
+              style={Object.assign(
+                { ...style },
+                isHovering ? { ...isHoveringStyles } : {}
+              )}
+              onMouseOver={handleMouseOver}
+              onMouseOut={handleMouseOver}
+            ></div>
+          )
+        }
+      )}
+    </div>
+  )
+}
